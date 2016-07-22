a912993ebd9677998df6348cc9ab6cd99458a508
FioFiyo <fioarlego1@msn.com>
2016-07-22 21:51:36 +0000

    admin only access to users added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,16 @@
 class Admin::UsersController < ApplicationController
+  
   def index
   	@users = User.all
+  	@user = User.find(session[:user_id])
+  	unless @user.is_admin?
+  	 redirect_to movies_path, notice: 'Access Denied'
+  	end
   end
 
   def show
+
   end
+
 
 end
